d56ef376bec0c095ae234ffa962d2c4e22ed5baa
Sebastián Claudio Nale <sebinale@gmail.com>
2022-11-15 21:15:25 -0300

    Caches contract data to reduce resource costs.
    This should improve response times too.

--- App/pages/api/abi.js
+++ App/pages/api/abi.js
@@ -2,8 +2,6 @@ import { loadDeployment } from "../../utils/loadDeployment";
 import { ethers } from "ethers";
 
 export default async function handler(req, res) {
-  console.log("REQ METHOD: ", req.method);
-
   const { wcb, token, abi } = await loadDeployment(ethers, process.env.ETH_NODE);
 
   res.status(200).json({
--- App/pages/api/getBets.js
+++ App/pages/api/getBets.js
@@ -7,63 +7,43 @@ import { inspect } from "util";
 const dogeDecimalsSignificantDivisor = ethers.BigNumber.from(10 ** 6);
 const twoDecimalsDivisor = ethers.BigNumber.from(10 ** 2);
 
+let fixedBetAmountMemoized;
+async function getFixedBetAmount(wcb) {
+  if (fixedBetAmountMemoized !== undefined) return fixedBetAmountMemoized;
+
+  fixedBetAmountMemoized = await wcb.FIX_BET_AMOUNT();
+  return fixedBetAmountMemoized;
+}
+
+let lastBlock;
+let betsCache;
+let jackpotCache;
+async function getBettingStatus(wcb, token) {
+  const currentBlock = await wcb.provider.getBlockNumber();
+  if (lastBlock === currentBlock) return { bets: betsCache, jackpot: jackpotCache };
+
+  {
+    [betsCache, jackpotCache] = await Promise.all([wcb.getBetsOfAllTeams(), token.balanceOf(wcb.address)]);
+  }
+  lastBlock = currentBlock;
+  return { bets: betsCache, jackpot: jackpotCache };
+}
+
 export default async function getBets(req, res) {
-  console.log("REQ METHOD", req.method);
-
-  const teams = [
-    { name: "Brazil", flag: "/countryFlags/br-flag.gif" },
-    { name: "Cameroon", flag: "/countryFlags/cm-flag.gif" },
-    { name: "Germany", flag: "/countryFlags/gm-flag.webp" },
-    { name: "France", flag: "/countryFlags/fr-flag.webp" },
-    { name: "Costa Rica", flag: "/countryFlags/cs-flag.webp" },
-    { name: "Spain", flag: "/countryFlags/sp-flag.webp" },
-    { name: "England", flag: "/countryFlags/en-flag.webp" },
-    { name: "Argentina", flag: "/countryFlags/ar.gif" },
-    { name: "Belgium", flag: "/countryFlags/be-flag.webp" },
-    { name: "Canada", flag: "/countryFlags/ca-flag.webp" },
-    { name: "Uruguay", flag: "/countryFlags/uy-flag.gif" },
-    { name: "Denmark", flag: "/countryFlags/da-flag.webp" },
-    { name: "Netherlands", flag: "/countryFlags/nl-flag.webp" },
-    { name: "Ghana", flag: "/countryFlags/gh-flag.gif" },
-    { name: "Poland", flag: "/countryFlags/pl-flag.webp" },
-    { name: "Ecuador", flag: "/countryFlags/ec-flag.webp" },
-    { name: "Portugal", flag: "/countryFlags/po-flag.gif" },
-    { name: "Switzerland", flag: "/countryFlags/sz-flag.webp" },
-    { name: "Wales", flag: "/countryFlags/wa-flag.png" },
-    { name: "Australia", flag: "/countryFlags/as-flag.webp" },
-    { name: "Croatia", flag: "/countryFlags/hr-flag.webp" },
-    { name: "Iran", flag: "/countryFlags/ir-flag.webp" },
-    { name: "Denmark", flag: "/countryFlags/da-flag.webp" },
-    { name: "Tunisia", flag: "/countryFlags/ts-flag.webp" },
-    { name: "Saudi Arabia", flag: "/countryFlags/sa-flag.webp" },
-    { name: "Senegal", flag: "/countryFlags/sg-flag.gif" },
-    { name: "Morocco", flag: "/countryFlags/mo-flag.gif" },
-    { name: "Qatar", flag: "/countryFlags/qa-flag.gif" },
-    { name: "Serbia", flag: "/countryFlags/ri-flag.gif" },
-    { name: "Mexico", flag: "/countryFlags/mx-flag.webp" },
-    { name: "South Korea", flag: "/countryFlags/ks-flag.webp" },
-    { name: "Japan", flag: "/countryFlags/ja-flag.webp" },
-  ];
-
-  console.log("entró a getBets");
-
   const { wcb, token } = await loadDeployment(ethers, process.env.ETH_NODE);
 
-  //Get fix bet amount.
   let fixedBetAmount;
+  let bets, jackpot;
   try {
-    fixedBetAmount = await wcb.FIX_BET_AMOUNT();
+    fixedBetAmount = await getFixedBetAmount(wcb);
+    ({ bets, jackpot } = await getBettingStatus(wcb, token));
   } catch (error) {
-    //If Fails to connect to contract, return failure to frontend
+    // Inform failure to frontend if the contract is unavailable
     console.error("CONTRACT ERROR: ", error);
     res.status(200).json({ contractStatus: "failure" });
     return;
   }
 
-  //Get bets per team
-  const bets = await wcb.getBetsOfAllTeams();
-
-  const jackpot = await token.balanceOf(wcb.address);
   const jackpotString = buildJackpotString(jackpot);
 
   const response = {
