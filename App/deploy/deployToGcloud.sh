5a6c1e93c70ad8c8fc7990cf2a46e5d89014bc54
wanidulf <danielwulf@gmail.com>
2022-11-16 13:55:09 +0000

    deploy script

---
+++ App/deploy/deployToGcloud.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o xtrace
+
+# Deploy to Goerli
+if [ $1 = 'goerli' ]
+then
+    wcbConfig="deploy/contentProdGoerli"
+    wcbProject="worldcup-goerli"
+
+#Deploy to Mainnet
+elif [ $1 = 'mainnet' ]
+then
+    wcbConfig="deploy/contentProdMainnet"
+    wcbProject="worldcup-goerli"
+else 
+echo "Invalid network"
+exit 1
+fi
+
+
+# Copy config content to env variables file
+cp -f "$wcbConfig" .env.production
+
+# Build the app
+npm run build 
+
+# Deploy to Gcloud
+gcloud app deploy --project "$wcbProject" --quiet
+    
+spd-say done
